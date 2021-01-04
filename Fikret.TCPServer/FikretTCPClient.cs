47e88dc80d0e3adc37bc4839efcbeae761c8ec23
Fikret Musk <54106274+wgex@users.noreply.github.com>
2021-01-04 13:37:34 +0300

    Initial Commit

---
+++ Fikret.TCPServer/FikretTCPClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+
+namespace Fikret.TCP
+{
+    class FikretTCPClient
+    {
+        private TcpClient tc = new TcpClient();
+
+        private NetworkStream ns;
+
+        public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs args);
+
+        public event EventHandler MessageReceived;
+
+        public void Send(string message)
+        {
+            byte[] mesaj = new byte[100];
+
+            mesaj = Encoding.UTF8.GetBytes(message + "{CONNECTION_END}");
+
+            ns.Write(mesaj, 0, mesaj.Length);
+        }
+
+        public void Start()
+        {
+            Timer tmr = new Timer();
+            tmr.Elapsed += kontrol;
+        }
+
+        private void kontrol(object sender, ElapsedEventArgs args)
+        {
+            try
+            {
+                byte[] cevap = new byte[100];
+                ns.Read(cevap, 0, cevap.Length);
+                if (cevap != null)
+                {
+                    string metin = Encoding.UTF8.GetString(cevap).Split(new string[] { "{CONNECTION_END}" }, StringSplitOptions.None)[0];
+
+                    MessageReceivedEventArgs eventargs = new MessageReceivedEventArgs(metin);
+                    
+                    MessageReceived(this, eventargs);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Fikret.TCPServer/FikretTCPServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Fikret.UDPServer
+{
+    public class UDPServer
+    {
+        public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs args);
+
+        TcpListener tcpserver;
+
+        NetworkStream sonstream;
+
+        public event EventHandler MessageReceived;
+
+        public void Start(int port)
+        {
+            tcpserver = new TcpListener(IPAddress.Any, port);
+            tcpserver.Start();
+            kabulet();
+        }
+
+        public void Stop()
+        {
+            tcpserver.Stop();
+        }
+
+        private void kabulet()
+        {
+            tcpserver.BeginAcceptTcpClient(handle, tcpserver);
+        }
+
+        private void handle(IAsyncResult result)
+        {
+            TcpClient client = tcpserver.EndAcceptTcpClient(result);
+            while (true)
+            {
+                kabulet();
+
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] mesaj = new byte[1024];
+
+                if (mesaj != null)
+                {
+                    ns.Read(mesaj, 0, mesaj.Length);
+
+                    string metin = Encoding.UTF8.GetString(mesaj).Split(new string[] { "{CONNECTION_END}" }, StringSplitOptions.None)[0];
+
+                    MessageReceivedEventArgs args = new MessageReceivedEventArgs(metin);
+
+                    sonstream = ns; //nice
+
+                    MessageReceived(this, args);
+                }
+            }
+        }
+
+        public void Send(string metin)
+        {
+            byte[] gndmesaj = new byte[100];
+            gndmesaj = Encoding.UTF8.GetBytes(metin + "{CONNECTION_END}");
+
+            sonstream.Write(gndmesaj, 0, gndmesaj.Length);
+        }
+    }
+
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
